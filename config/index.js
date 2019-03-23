4b5fe6cb1709c15f01a5178bf5e0a20ddf2eced0
Hector Trinidad <hmtv16@gmail.com>
2019-03-22 21:06:59 -0600

    config base de datos

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  database_url: process.env.STRING_CONNECTION || 'mongodb://localhost:27017/prueba-heroku'
 }
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const config = require('./config')
+const mongoose = require('mongoose')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({"Api":"Esta es primera llamada, primera!"})
+mongoose.connect(config.database_url, { useNewUrlParser: true })
+  .then(
+    () => {
+      console.log('Database Ready For Action')
+    },
+    err => {
+      console.error(err)
+    }
+  )
+
+app.all('/', (req, res) => {
+  res.json({
+    Api:'Esta es primera llamada, primera!',
+    Version: '0.0.1'
+})
 });  
 
 app.listen(config.port, () => {
-  console.log(`Api ready on ${config.port}`)
+  console.log(`API ready on ${config.port}`)
 });
